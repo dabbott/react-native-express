1d8ec659281ac0b1e5385aed689b5832bbe2bbeb
Daniel Garcia <dgarcia@blackphone.ch>
2017-01-30 16:44:46 +0100

    Fix height value in TimingDriver example

--- src/pages/ShoutemUIAnimation.js
+++ src/pages/ShoutemUIAnimation.js
@@ -77,7 +77,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 })
 
