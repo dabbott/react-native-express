e60915390cafdf83fe9ab2d9b6a1a24c254b9965
Devin Abbott <devinabbott@gmail.com>
2017-05-16 07:02:55 +0200

    Update FlatList, SectionList, and ListView pages

--- src/components/SnackPlayer.js
+++ src/components/SnackPlayer.js
@@ -1,29 +1,32 @@
 import React, { Component, PropTypes } from 'react'
+import createStyles from 'react-styles-provider'
 
+@createStyles({
+  player: {
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    background: '#fafafa',
+    border: '1px solid rgba(0,0,0,.16)',
+    borderRadius: 4,
+    height: 512,
+  },
+})
 export default class SnackPlayer extends Component {
+
   componentDidMount() {
-    window.ExpoSnack && window.ExpoSnack.initialize();
+    window.ExpoSnack && window.ExpoSnack.initialize()
   }
 
   render() {
-    const {id, platform, preview} = this.props
+    const {styles, id, platform, preview} = this.props
 
     return (
-      <div>
-        <div
-          data-snack-id={id}
-          data-snack-platform={platform || "ios"}
-          data-snack-preview={preview || "true"}
-          style={{
-            overflow: 'hidden',
-            background: '#fafafa',
-            border: '1px solid rgba(0,0,0,.16)',
-            borderRadius: '4px',
-            height: '505px',
-            width: '100%',
-          }}
-        />
-      </div>
+      <div
+        data-snack-id={id}
+        data-snack-platform={platform || "ios"}
+        data-snack-preview={preview || "true"}
+        style={styles.player}
+      />
     )
   }
 }
