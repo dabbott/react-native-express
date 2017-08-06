673276cb8dfb693c713e17b61fb2d69a1568714e
Devin Abbott <devinabbott@gmail.com>
2017-08-06 12:07:36 -0700

    Udpate react-apollo in GraphQL example

--- src/pages/GraphQL.js
+++ src/pages/GraphQL.js
@@ -7,10 +7,7 @@ import { WebPlayer, PageHeader } from '../components'
 
 const simpleInstagramApp = `import React, { Component } from 'react'
 import { AppRegistry, Image, View, ActivityIndicator, StyleSheet } from 'react-native'
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
-import { ApolloProvider, graphql } from 'react-apollo'
-
-import gql from 'graphql-tag'
+import { ApolloProvider, ApolloClient, createNetworkInterface, gql, graphql } from 'react-apollo'
 
 // Initialize the Apollo Client
 const client = new ApolloClient({
@@ -89,16 +86,14 @@ AppRegistry.registerComponent('App', () => App)
 `
 
 const vendorComponents = [
-  ['apollo-client', 'https://cdn.rawgit.com/dabbott/665a982d2de9b7eea22114e266b1474b/raw/e289e526f402989f0c42668dcefe2b15127ca3a1/apollo-client.js'],
-  ['react-apollo', 'https://cdn.rawgit.com/dabbott/c1dcec0ac74dc7d14e5a3a78d5c7fdd3/raw/f20b6af16aac7bff1808fea76eb9f815e41f8e96/react-apollo.js'],
-  ['graphql-tag', 'https://gist.githubusercontent.com/dabbott/bf832015419ee30344ac573705c2cbeb/raw/6bfcd7294d324aeaf117f6b8bde40715bc89ec78/graphql-tag']
+  ['react-apollo', 'https://gist.githubusercontent.com/dabbott/344ef01de49ab7e871774e773e1048bc/raw/498dd3756eeeffb697dfe0cd8fd0b359b81c0f5d/react-apollo.js']
 ]
 
 
-const content = markdown(markdownOptions)`
-[GraphQL](http://graphql.org/) is a *query language* for APIs. It enables declarative data fetching and thus ties in perfectly with React as a declarative framework for building user interfaces. GraphQL can either complement or entirely replace the usage of REST APIs. 
+const content = markdown(markdownOptions) `
+[GraphQL](http://graphql.org/) is a *query language* for APIs. It enables declarative data fetching and thus ties in perfectly with React as a declarative framework for building user interfaces. GraphQL can either complement or entirely replace the usage of REST APIs.
 
-The main difference between REST and GraphQL is that RESTful APIs have *multiple endpoints* that return *fixed data structures* whereas a GraphQL server only exposes a *single endpoint* and returns *flexible data structures*. This works because a client that needs data from the server also submits its precise data requirements in each request which allows the server to tailor the response exactly according to the client’s needs. 
+The main difference between REST and GraphQL is that RESTful APIs have *multiple endpoints* that return *fixed data structures* whereas a GraphQL server only exposes a *single endpoint* and returns *flexible data structures*. This works because a client that needs data from the server also submits its precise data requirements in each request which allows the server to tailor the response exactly according to the client’s needs.
 
 You can learn more about the differences between GraphQL and REST [here](https://www.howtographql.com/basics/1-graphql-is-the-better-rest/). To get a high-level overview and understand more about the architectural use cases of GraphQL, take a look at [this](https://www.howtographql.com/basics/3-big-picture/) article.
 
@@ -115,6 +110,8 @@ For an in-depth learning experience, visit the [How to GraphQL](https://www.howt
 ## Minimal Example
 
 Here is a minimal example that demonstrates how to load data using [Graphcool](https://www.graph.cool/) and [Apollo Client](http://dev.apollodata.com/react/). If you want to understand more about how it works, check out the following chapters.
+
+Note that the example will throw an error if you have the Redux DevTools Chrome extension enabled!
 
 ${
   <WebPlayer
