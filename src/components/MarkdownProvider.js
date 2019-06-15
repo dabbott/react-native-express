b6ae0a4ab26c6990e4ae33bc2511e1999dffe3d5
Devin Abbott <devinabbott@gmail.com>
2019-06-14 19:07:58 -0700

    Import MDXProvider from correct package

--- src/components/MarkdownProvider.js
+++ src/components/MarkdownProvider.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MDXProvider } from '@mdx-js/tag'
+import { MDXProvider } from '@mdx-js/react'
 import SectionHeader from './SectionHeader'
 import Paragraph from './Paragraph'
 
