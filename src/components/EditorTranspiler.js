ab6dc76b6d316e65c8bec3434e2bce84aef5b90c
Devin Abbott <devinabbott@gmail.com>
2019-06-14 19:49:46 -0700

    Convert pages

---
+++ src/components/EditorTranspiler.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import WebPlayer from './WebPlayer'
+
+export default class EditorTranspiler extends Component {
+  shouldComponentUpdate() {
+    return false
+  }
+
+  render() {
+    return (
+      <WebPlayer
+        height={340}
+        fullscreen={false}
+        showTranspiler={true}
+        transpilerTitle={'Output compiled with Babel'}
+        {...this.props}
+      />
+    )
+  }
+}
---
+++ src/examples/block_scoped_declarations.js
@@ -0,0 +1,15 @@
+const code = `const a = 1
+let b = 'foo'
+
+// Not allowed!
+// a = 2
+
+// Ok!
+b = 'bar'
+
+if (true) {
+  const a = 3
+}`
+const title = 'Using const and let'
+
+export default { code, title }
