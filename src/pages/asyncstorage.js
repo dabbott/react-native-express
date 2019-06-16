cb8979f7834ea2cee2bcb29654894c0a9df74119
Devin Abbott <devinabbott@gmail.com>
2019-06-16 15:55:13 -0700

    Convert persistance pages

---
+++ src/pages/asyncstorage.js
@@ -0,0 +1,70 @@
+const indexFile = `import React, { Component } from 'react'
+import { AppRegistry, View, Text, AsyncStorage, StyleSheet } from 'react-native'
+
+import Input from './Input'
+
+const STORAGE_KEY = 'ASYNC_STORAGE_NAME_EXAMPLE'
+
+export default class App extends Component {
+
+  state = {name: 'World'}
+
+  componentWillMount() {
+    this.load()
+  }
+
+  load = async () => {
+    try {
+      const name = await AsyncStorage.getItem(STORAGE_KEY)
+
+      if (name !== null) {
+        this.setState({name})
+      }
+    } catch (e) {
+      console.error('Failed to load name.')
+    }
+  }
+
+  save = async (name) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, name)
+
+      this.setState({name})
+    } catch (e) {
+      console.error('Failed to save name.')
+    }
+  }
+
+  render() {
+    const {name} = this.state
+
+    return (
+      <View>
+        <Input
+          placeholder={'Type your name, hit enter, and refresh!'}
+          onSubmitEditing={this.save}
+        />
+        <Text style={styles.text}>
+          Hello {name}!
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    padding: 15,
+    backgroundColor: 'skyblue',
+  },
+})
+
+AppRegistry.registerComponent('App', () => App)
+`
+
+const files = [
+  ['index.js', indexFile],
+  ['Input.js', require('!!raw-loader!../examples/Input')],
+]
+
+export default { files }
---
+++ src/pages/redux_persist.js
@@ -0,0 +1,106 @@
+const indexFile = `import { AppRegistry, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
+
+// Import the reducer and create a store
+import { reducer } from './todoListRedux'
+
+// Add the autoRehydrate middleware to your redux store
+const store = createStore(reducer, undefined, autoRehydrate())
+
+// Enable persistence
+persistStore(store)
+
+// Import the App container component
+import App from './App'
+
+// Pass the store into the Provider
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent('App', () => AppWithStore)
+`
+
+const appFile = `import React, { Component } from 'react'
+import { AppRegistry, View } from 'react-native'
+import { connect } from 'react-redux'
+
+import { actionCreators } from './todoListRedux'
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+})
+
+class App extends Component {
+
+  onAddTodo = (text) => {
+    const {dispatch} = this.props
+
+    dispatch(actionCreators.add(text))
+  }
+
+  onRemoveTodo = (index) => {
+    const {dispatch} = this.props
+
+    dispatch(actionCreators.remove(index))
+  }
+
+  render() {
+    const {todos} = this.props
+
+    return (
+      <View>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </View>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(App)
+`
+
+const files = [
+  ['index.js', indexFile],
+  ['todoListRedux.js', require('!!raw-loader!../examples/TodoListRedux')],
+  ['App.js', appFile],
+  ['List.js', require('!!raw-loader!../examples/List')],
+  ['Input.js', require('!!raw-loader!../examples/Input')],
+  ['Title.js', require('!!raw-loader!../examples/Title')],
+]
+
+const vendorComponents = [
+  [
+    'redux',
+    'Redux',
+    'https://cdnjs.cloudflare.com/ajax/libs/redux/3.7.2/redux.min.js',
+  ],
+  [
+    'react-redux',
+    'ReactRedux',
+    'https://cdnjs.cloudflare.com/ajax/libs/react-redux/5.0.7/react-redux.min.js',
+  ],
+  [
+    'redux-persist',
+    'redux-persist',
+    'https://cdnjs.cloudflare.com/ajax/libs/redux-persist/4.0.0-alpha7/redux-persist.js',
+  ],
+]
+
+export default { files, vendorComponents }
