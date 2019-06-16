824a676922e2b4a7293aee82cfa9a924a2a3352c
Devin Abbott <devinabbott@gmail.com>
2019-06-15 18:05:22 -0700

    Convert pages

---
+++ src/pages/image.js
@@ -0,0 +1,23 @@
+const code = `import React, { Component } from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Image
+        style={styles.image}
+        source={{uri: 'http://www.reactnativeexpress.com/logo.png'}}
+      />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+})
+`
+
+export default { code }
