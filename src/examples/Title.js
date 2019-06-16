9205abc4b3ae931f975a05fa1e1b5ad1d5e94424
Devin Abbott <devinabbott@gmail.com>
2019-06-16 15:09:59 -0700

    Convert pages

---
+++ src/examples/Title.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class Title extends Component {
+  render() {
+    const { children } = this.props
+
+    return (
+      <View style={styles.header}>
+        <Text style={styles.title}>{children}</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'skyblue',
+    padding: 15,
+  },
+  title: {
+    textAlign: 'center',
+    color: 'white',
+  },
+})
---
+++ src/pages/data_component_state.js
@@ -0,0 +1,60 @@
+const appFile = `import React, { Component } from 'react'
+import { AppRegistry, View } from 'react-native'
+
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
+export default class App extends Component {
+
+  state = {
+    todos: ['Click to remove', 'Learn React Native', 'Write Code', 'Ship App'],
+  }
+
+  onAddTodo = (text) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: [text, ...todos],
+    })
+  }
+
+  onRemoveTodo = (index) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: todos.filter((todo, i) => i !== index),
+    })
+  }
+
+  render() {
+    const {todos} = this.state
+
+    return (
+      <View>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </View>
+    )
+  }
+}
+`
+
+const files = [
+  // ['index.js', require('!!raw!../examples/IndexRegisterApp')],
+  ['App.js', appFile],
+  ['List.js', require('!!raw-loader!../examples/List')],
+  ['Input.js', require('!!raw-loader!../examples/Input')],
+  ['Title.js', require('!!raw-loader!../examples/Title')],
+]
+
+export default { files }
