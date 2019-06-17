f7e1ae6a6faf8bd3ab899877620cb55424c8c331
Devin Abbott <devinabbott@gmail.com>
2019-06-16 18:15:32 -0700

    Rearrange pages, improve styles for build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,6 @@
+import '../styles/reset.css'
+import '../styles/main.css'
+
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
--- src/utils/isMobile.js
+++ src/utils/isMobile.js
@@ -1,6 +1,6 @@
 // Primitive mobile detection
 const detectMobile = () =>
-  navigator &&
+  typeof navigator !== 'undefined' &&
   navigator.userAgent &&
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
