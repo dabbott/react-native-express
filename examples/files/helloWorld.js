04fdcb9658d410618b7f9c4476417508e4b8a1be
Devin Abbott <devinabbott@gmail.com>
2020-08-22 09:40:06 -0700

    Fix safari layout issue

--- examples/files/helloWorld.js
+++ examples/files/helloWorld.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Hello, world!</Text>
-  </View>
-)
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
