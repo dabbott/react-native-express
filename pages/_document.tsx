4a31d857d7cb01b2b5664d4c6fccd0930810f95a
Devin Abbott <devinabbott@gmail.com>
2020-12-29 10:31:18 -0800

    Enable strict typing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import guidebook from '../guidebook'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
